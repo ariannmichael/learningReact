080ae5ca31ae9db0fe81742dd150009a10943c00
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-02-19 02:22:05 -0300

    integração react-redux

---
+++ todo-app/frontend/src/todo/todoActions.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const changeDescription = event => ({
+    type: 'DESCRIPTION_CHANGED',
+    payload: event.target.value
+})
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SHEARCHED',
+        payload: request
+    }
+}

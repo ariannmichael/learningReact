6d290e6a5a69dd33a0f2059c4742a8e256884315
ariannmichael <chael.andrade@gmail.com>
2019-02-26 18:20:33 -0300

    Pai e Filho #2

--- react16/exercicios/src/componentes/Pai.jsx
+++ react16/exercicios/src/componentes/Pai.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Filho from './Filho'
+import {childrenWithProps} from '../utils/utils'
+
 
 export default props => (
     <div>
         <h1>{props.name} {props.sobrenome}</h1>
         <h2>Filhos</h2>
         <ul>
-            <Filho nome="Pedro" sobrenome={props.sobrenome}/>
-            <Filho {...props}/>
-            <Filho {...props} nome="Carla"/>
+            {childrenWithProps(props)}
         </ul>
     </div>
 )
--- react16/exercicios/src/index.js
+++ react16/exercicios/src/index.js
@@ -5,12 +5,16 @@ import BomDia from './componentes/BomDia'
 import {BoaTarde, BoaNoite} from './componentes/Multiplos'
 import Saudacao from './componentes/Saudacao'
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 
 const elemento = <h1>React!</h1>
 
 ReactDOM.render(<div>
     <Pai nome="Paulo" sobrenome="Silva">
-        {/* Como passo os componentes filhos aqui */}
+        <Filho nome="Pedro" />
+        <Filho nome="Carol" />
+        <Filho nome="Carla"/>
+
     </Pai>
 </div>, document.getElementById('root'))

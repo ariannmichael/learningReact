a147985855be1367fdaf427f52a79174026622bc
ariannmichael <chael.andrade@gmail.com>
2019-02-26 17:55:20 -0300

    Pai e Filho #1

--- react16/exercicios/src/index.js
+++ react16/exercicios/src/index.js
@@ -4,10 +4,13 @@ import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
 import {BoaTarde, BoaNoite} from './componentes/Multiplos'
 import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
 
 
 const elemento = <h1>React!</h1>
 
 ReactDOM.render(<div>
-    <Saudacao/>
+    <Pai nome="Paulo" sobrenome="Silva">
+        {/* Como passo os componentes filhos aqui */}
+    </Pai>
 </div>, document.getElementById('root'))

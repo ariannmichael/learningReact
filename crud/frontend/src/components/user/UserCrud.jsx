c1aeebd336b5fdeb2323781e100cb9ac6eb9dd79
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-03-02 03:06:58 -0300

    crud rotas

---
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Main from '../templates/Main'
+
+
+const headerProps = {
+    icon: 'users',
+    title: 'Usuários',
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Exlcuir'
+}
+
+export default class UserCrud extends Component {
+    render() {
+        return(
+            <Main {...headerProps}>
+                Cadastro de Usuários
+            </Main>
+        )
+    }
+}
